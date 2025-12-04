0beac668dcaaca498f39cd1e214573043a3c61f7
Maarten <maartenvanhove@live.nl>
2025-12-04 14:30:40 +0100

    Completed first challange!

--- 2025/day01/solution.py
+++ 2025/day01/solution.py
@@ -1,1 +1,26 @@
-print("Hello World!")
+def main():
+    txtFile = open('2025/day01/input.txt')
+
+    startNumber = 50
+    dialNumber = startNumber
+    zeroCounter = 0
+
+    print(f'startNumber: {dialNumber}')
+
+    for line in txtFile:
+        dial = line.strip()
+        distance = int(dial[1:])
+        if(dial[0] == 'L'):
+            dialNumber -= distance
+        elif(dial[0] == 'R'):
+            dialNumber += distance
+
+        dialNumber %= 100
+        print(dialNumber)
+
+        if(dialNumber == 0):
+            zeroCounter += 1
+            
+    print(f'Zero counter: {zeroCounter}')
+
+main()
