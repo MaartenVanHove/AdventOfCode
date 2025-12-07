174b7b68addbb6f66c76fdfaaaed6c8433ecdf0b
Maarten <maartenvanhove@live.nl>
2025-12-07 18:11:10 +0100

    found first falses ids based on if the first half is identical to the second half

--- 2025/day02/part01/solution.py
+++ 2025/day02/part01/solution.py
@@ -1,5 +1,26 @@
 def main():
     txtFile = open('2025/day02/input.txt')
-    print(txtFile.read())
+
+    invalidIdCount = 0
+
+    IdRanges = txtFile.read().split(',')
+
+    for idRange in IdRanges:
+        idStartAndEnd = idRange.split('-')
+        idStart = int(idStartAndEnd[0])
+        idEnd = int(idStartAndEnd[1])
+        rangeOfIds = int(idEnd - idStart)
+        # print(f'Total range: {idRange}')
+        # print(f'Start: {idStart}')
+        # print(f'End: {idEnd}')
+        # print(f'Range: {rangeOfIds}')
+        for step in range(rangeOfIds):
+            id = str(idStart + step)
+            idLength = int(len(id))
+            half = idLength // 2
+            if(id[0:half] == id[half:]):
+                invalidIdCount += 1
+   
+    print(f"Count of invalid ID's: {invalidIdCount}")
 
 main()
