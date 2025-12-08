973c9c161132a97cf152906f56e1e722f519d424
Maarten <maartenvanhove@live.nl>
2025-12-08 09:59:36 +0100

    Finished Day 2 - Task 1

--- 2025/day02/part01/solution.py
+++ 2025/day02/part01/solution.py
@@ -1,25 +1,24 @@
 def main():
     txtFile = open('2025/day02/input.txt')
-
     invalidIdCount = 0
 
-    IdRanges = txtFile.read().split(',')
+    idRanges = txtFile.read().strip().split(',')
 
-    for idRange in IdRanges:
+    for idRange in idRanges:
         idStartAndEnd = idRange.split('-')
         idStart = int(idStartAndEnd[0])
         idEnd = int(idStartAndEnd[1])
-        rangeOfIds = int(idEnd - idStart)
-        # print(f'Total range: {idRange}')
-        # print(f'Start: {idStart}')
-        # print(f'End: {idEnd}')
-        # print(f'Range: {rangeOfIds}')
-        for step in range(rangeOfIds):
-            id = str(idStart + step)
+
+        for num in range(idStart, idEnd + 1):
+            id = str(num)
+
+            if (len(id) % 2 != 0): continue
+
             idLength = int(len(id))
+
             half = idLength // 2
             if(id[0:half] == id[half:]):
-                invalidIdCount += 1
+                invalidIdCount += int(id)
    
     print(f"Count of invalid ID's: {invalidIdCount}")
 
